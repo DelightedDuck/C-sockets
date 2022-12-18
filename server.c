3f7ed31b8cea6889921ae87466a012a978808cd6
DelightedDuck <84408907+DelightedDuck@users.noreply.github.com>
2022-12-18 12:34:21 +0000

    error with sockaddr and bzero

--- server.c
+++ server.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
 
 int main() {
 	int sockfd;
-
-	struct sockaddr_in { 
-		short sin_family; // e.g. AF_INET 
-		unsigned short sin_port; // e.g. htons(3490)‏ 
-		struct in_addr sin_addr; // see struct in_addr below 
-		char sin_zero[8]; // zero this if you want to 
-	}; 
-
-	struct in_addr { 
-		unsigned long s_addr; // load with inet_aton()‏ 
-	};
-
-	printf("Hello World \n");
-
+	const int MAXLINE = 4096;
+	const int PORT = 69;
+	
 	// Create socket
 	if ((sockfd = socket (AF_INET, SOCK_STREAM, 0) < 0)) {
 		printf ("Error while creating the socket\n");
@@ -26,25 +16,24 @@ int main() {
 	};
 
 	bzero (&my_addr, sizeof(my_addr));
-	my_addr.sin_family = AF_INET; // match the socket() call
-	my_addr.sin_port = htons(5100); // specify port to listen on
-	my_addr.sin_addr.s_addr = htonl(INADDR_ANY); //allow the server to accept a client connection on any interface
+	sockaddr.sin_family = AF_INET; 
+	sockaddr.sin_port = htons(PORT);
+	//	sockaddr.sin_addr.s_addr = htonl(INADDR_ANY); //allow the server to accept a client connection on any interface
 
- 
 	// Bind
-	if ((bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) > 0)) {
+	if ((bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) > 0)) {
 		printf("error while binding");
 		return -1;
 	}
 	
 	// Listen
-	if ((listen (int sockfd, int backlog) > 0)) {
+	if ((listen(sockfd, MAXLINE) > 0)) {
 		printf("failed to listen");
 		return -1;
 	}
 	
 	// Accept
-	if ((accept (int sockfd, struct sockaddr *fromaddr, socklen_t *addrlen) > 0)) {
+	if ((accept(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) > 0)) {
 		printf("failed to accept");
 		return -1;
 	}
